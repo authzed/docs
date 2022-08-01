2ab7cbddfd497ba0903f0c24bb486097f955aea3
Joseph Schorr <josephschorr@users.noreply.github.com>
2022-08-01 12:19:58 -0400

    Upgrade docs library and remove min-width on inline playgrounds

--- src/components/InlinePlayground.tsx
+++ src/components/InlinePlayground.tsx
@@ -23,6 +23,6 @@ import React from "react";
 export function InlinePlayground(props: { reference: string, children: any[] }) {
     let playgroundUrl = 'https://play.authzed.com';
     return <div>
-        <iframe style={{ minWidth: '800px', width: '100%', border: '0px', height: '500px' }} src={`${playgroundUrl}/i/${props.reference}`}></iframe>
+        <iframe style={{ width: '100%', border: '0px', height: '500px' }} src={`${playgroundUrl}/i/${props.reference}`}></iframe>
     </div>
 }
