3d31b9549f06935c86673b6f57783197ac158d8d
Sam Kim <sam@authzed.com>
2024-02-22 10:59:36 -0800

    Add banner component

---
+++ components/banner.tsx
@@ -0,0 +1,17 @@
+import { usePathname } from 'next/navigation';
+
+export default function Banner() {
+  const pathname = usePathname();
+  const isCommercial = pathname.startsWith('/authzed/');
+
+  return isCommercial ? (
+    <a href="https://authzed.com/zanzibar?utm_source=docs">
+      📄 Have you read Google's Zanzibar paper? We annotated it with additional
+      context and comparisons with SpiceDB ↗
+    </a>
+  ) : (
+    <a href="https://github.com/authzed/spicedb">
+      SpiceDB is 100% open source. Please help us by starring our GitHub repo. ↗
+    </a>
+  );
+}
