45d22f1dad21c9e16f931c256303de026459eb9b
Maria Ines Parnisari <maineparnisari@gmail.com>
2025-06-02 19:48:53 -0700

    add swagger doc

---
+++ components/swagger.tsx
@@ -0,0 +1,17 @@
+import dynamic from 'next/dynamic'
+import type { SwaggerUIProps } from 'swagger-ui-react'
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react') as Promise<{ default: React.ComponentType<SwaggerUIProps> }>, { ssr: false })
+
+import 'swagger-ui-react/swagger-ui.css'
+
+export function Swagger(props: {}) {
+    return (<SwaggerUI
+        url="https://raw.githubusercontent.com/authzed/api/refs/heads/main/docs/apidocs.swagger.json"
+        deepLinking={true}
+        supportedSubmitMethods={[]}
+        tryItOutEnabled={false}
+        defaultModelsExpandDepth={-1}
+        defaultModelRendering={'model'}
+    />)
+}
