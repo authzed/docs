e0914da5b9e21862663a9f4293a3af2b15e53de1
Sohan Maheshwar <1119120+sohanmaheshwar@users.noreply.github.com>
2026-01-28 18:40:06 +0100

    added langchain page for integrations

--- app/spicedb/integrations/_meta.ts
+++ app/spicedb/integrations/_meta.ts
@@ -1,4 +1,6 @@
 export default {
+  "langchain-spicedb":
+    "Use SpiceDB with LangChain & LangGraph for RAG & AI Agent Authorization",
   pinecone: "Access Control in Pinecone with SpiceDB",
   testcontainers: "Testing RAG Pipelines with Testcontainers",
 };
--- app/spicedb/ops/_meta.ts
+++ app/spicedb/ops/_meta.ts
@@ -7,8 +7,6 @@ export default {
   resilience: "Improving Resilience",
   observability: "Observability Tooling",
   "load-testing": "Load Testing",
-  "spicedb-langchain-langgraph-rag":
-    "Secure your RAG Pipelines using LangChain & LangGraph",
   "ai-agent-authorization":
     "Tutorial: Authorization for AI Agents using SpiceDB",
   "secure-rag-pipelines": "Tutorial: Securing RAG Pipelines with SpiceDB",
