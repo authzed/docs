c5ffe908a50e735d096aeb6864bc71c59a5e4863
Sohan Maheshwar <1119120+sohanmaheshwar@users.noreply.github.com>
2026-02-04 19:20:24 +0100

    added tutorials section

--- app/spicedb/integrations/_meta.ts
+++ app/spicedb/integrations/_meta.ts
@@ -1,6 +1,6 @@
 export default {
   "langchain-spicedb":
     "Use SpiceDB with LangChain & LangGraph for RAG & AI Agent Authorization",
-  pinecone: "Access Control in Pinecone with SpiceDB",
+  pinecone: "Access Control in RAG with Pinecone and SpiceDB",
   testcontainers: "Testing RAG Pipelines with Testcontainers",
 };
---
+++ app/spicedb/tutorials/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  "ai-agent-authorization":
+    "Tutorial: Authorization for AI Agents using SpiceDB",
+  "secure-rag-pipelines": "Tutorial: Securing RAG Pipelines with SpiceDB",
+};
